db499196aba4447500e181661f3e20bb0ea129fa
Austin Schrader <austin.jschrader@gmail.com>
2020-10-21 14:52:31 -0700

    Separate the user interface and business logic
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business Logic
 const add = function (number1, number2) {
 	return number1 + number2;
 };
@@ -13,3 +14,11 @@ const multiply = function (number1, number2) {
 const divide = function (number1, number2) {
 	return number1 / number2;
 };
+
+// User Interface Logic
+$(document).ready(function () {
+	const number1 = parseInt(prompt('Enter a number:'));
+	const number2 = parseInt(prompt('Enter another number:'));
+
+	alert(add(number1, number2));
+});
