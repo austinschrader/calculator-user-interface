6c034fbfa78ca13cd9d118b7fea2f0a21a522187
Austin Schrader <austin.jschrader@gmail.com>
2020-10-21 14:45:57 -0700

    Initialize the repository
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-$(document).ready(function () {});
+const add = function (number1, number2) {
+	return number1 + number2;
+};
+
+const subtract = function (number1, number2) {
+	return number1 - number2;
+};
+
+const multiply = function (number1, number2) {
+	return number1 * number2;
+};
+
+const divide = function (number1, number2) {
+	return number1 / number2;
+};
