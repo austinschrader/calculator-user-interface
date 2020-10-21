568a5ed02b6e27eb169e46ba8c8b0e3efc0623a2
Austin Schrader <austin.jschrader@gmail.com>
2020-10-21 15:26:56 -0700

    Fixed working version
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,10 @@ const divide = function (number1, number2) {
 $(document).ready(function () {
 	$('form#add').submit(function (event) {
 		event.preventDefault();
-		const number1 = parseInt('#add1').val();
-		const number2 = parseInt('#add2').val();
+		const number1 = parseInt($('#add1').val());
+		const number2 = parseInt($('#add2').val());
 
-		alert(add(number1, number2));
+		const result = add(number1, number2);
+		$('#output').text(result);
 	});
 });
